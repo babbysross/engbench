908104aa81b232f45ac55d80005d19c2855aeae2
solisares <rsg.elias@gmail.com>
2026-04-21 21:48:48 +0100

    some more screwing around

--- src/main.c
+++ src/main.c
@@ -8,17 +8,20 @@
 #include <form.h>
 
 #define BASIC_ELEMENTS 5
+
+int y, x;
+
+int submenu( int do_window );
+
 int menu_basic( int do_window ) {
 
     // Local variables
-    int x, y, ch, done;
+    int ch, done;
 	int itemwidth = 0;
     int hpad = 12; //NOT WORKING
     ITEM *basic_items[BASIC_ELEMENTS + 1];
     MENU *basic_menu;
     WINDOW *win;
-
-	getmaxyx(stdscr, y, x); //get window size
 
     // Create the basic items, menu
     basic_items[0] = new_item("1. |", "Description 1");
@@ -85,6 +88,89 @@ int menu_basic( int do_window ) {
 			case '\n':
             case '\r':
 				mvwprintw(win, 9, 1, "Opening submenu...            ");
+                endwin();
+                refresh();
+                submenu(1);
+				break;
+            case 'q': // Quit
+                done = 1;
+                break;
+        }
+
+        // Do a refresh
+        if ( do_window == 1 ) {
+            wrefresh(win);
+        } else {
+            refresh();
+        }
+    }
+
+    // Cleanup the menu
+    unpost_menu(basic_menu);
+    for (int i=0; i<BASIC_ELEMENTS; i++) {
+        free_item(basic_items[i]);
+    }
+    free_menu(basic_menu);
+
+    // Return successfully
+    return 0;
+}
+
+int submenu( int do_window ) {
+
+    // Local variables
+    int ch, done;
+	int itemwidth = 0;
+    int hpad = 12; //NOT WORKING
+    ITEM *basic_items[BASIC_ELEMENTS + 1];
+    MENU *basic_menu;
+    WINDOW *win;
+
+    // Create the basic items, menu
+    basic_items[0] = new_item("1. |", "Submenu 1");
+    basic_items[1] = new_item("2. |", "Submenu 2");
+    basic_items[2] = new_item("3. |", "Submenu 3");
+    basic_items[3] = new_item("4. |", "Submenu 4");
+    basic_items[4] = new_item("5. |", "Submenu 5");
+    basic_items[5] = NULL;
+    basic_menu = new_menu((ITEM **)basic_items);
+
+    // Add some color to the menu
+    start_color();
+    init_pair(1, COLOR_RED, COLOR_BLACK);
+    init_pair(2, COLOR_BLUE, COLOR_BLACK);
+    init_pair(3, COLOR_WHITE, COLOR_BLACK);
+    set_menu_fore(basic_menu, COLOR_PAIR(1) | A_REVERSE);
+    set_menu_back(basic_menu, COLOR_PAIR(2));
+    set_menu_grey(basic_menu, COLOR_PAIR(3));
+
+    win = newwin(y, x, 1, 1);
+    box(win, 0, 0);
+    set_menu_win(basic_menu, win);
+    set_menu_sub(basic_menu, derwin(win, y - 20, x - 40, 3, 1));
+    wmove(win, 1, 1);
+    keypad(win, TRUE);
+    mvwprintw(win, 1, 1, "Structural Steel");
+    post_menu(basic_menu);
+    wrefresh(win);
+
+    // Get input for the menu
+    done = 0;
+    while ( !done ) {
+        ch = do_window ? wgetch(win) : getch();
+        switch (ch) {
+            case KEY_DOWN:
+            case 's': // s is up:
+                mvwprintw(win, 9, 1, "You pressed the down arrow key");
+                menu_driver(basic_menu, REQ_DOWN_ITEM);
+                break;
+            case 'w': // w is up
+            case KEY_UP:
+                mvwprintw(win, 9, 1, "You pressed the up arrow key  "); //whitespace is a bodge to prevent text overlap
+                menu_driver(basic_menu, REQ_UP_ITEM);
+                break;
+			case '\n':
+            case '\r':
 				break;
             case 'q': // Quit
                 done = 1;
@@ -119,6 +205,7 @@ int main(void) {
 	curs_set(2);
 
 // do stuff
+    getmaxyx(stdscr, y, x);
 	menu_basic(1);
 
 // curses shutdown
